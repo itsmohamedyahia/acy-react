7c64551c71d906e53869b5f9fbd9a56cca77e9ba
itsmohamedyahia <justmyahia@gmail.com>
2023-05-04 00:58:17 +0300

    update root and fix bug in root

--- src/assets/Root.tsx
+++ src/assets/Root.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Components/Includes/Sidebar";
 import "./Root.css";
+import Navbar from "./Components/Includes/Navbar";
 
 function Root() {
   return (
     <div className="root-layout">
       <Sidebar />
+      <Navbar />
       <Outlet />
     </div>
   );
