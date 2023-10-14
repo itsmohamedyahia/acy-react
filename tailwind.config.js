e8903180642e848e8cbcb000ff9663c2f110bb53
itsmohamedyahia <justmyahia@gmail.com>
2023-10-14 19:51:38 +0300

    configure new fontSizes tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {}
+  theme: {  
+    fontSize: {
+      sm: '1.25rem',
+      base: '1.575rem',
+      lg: '1.953rem',
+      xl: '2.441rem',
+      '2xl': '3rem',
+      '3xl': '3.8rem',
+      '4xl': '4.768rem',
+      '5xl': '5.96rem',
+    },
+    extend: {}, 
   },
   plugins: []
 };
