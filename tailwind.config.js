275098fb475090e38250d67c9a74c9f7f5e9ef4d
itsmohamedyahia <justmyahia@gmail.com>
2023-10-15 13:30:54 +0300

    back to default font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {  
-    fontSize: {
-      sm: '1.25rem',
-      base: '1.575rem',
-      lg: '1.953rem',
-      xl: '2.441rem',
-      '2xl': '3rem',
-      '3xl': '3.8rem',
-      '4xl': '4.768rem',
-      '5xl': '5.96rem',
-    },
-    extend: {}, 
-  },
+    extend: {},
+},
   plugins: []
 };
