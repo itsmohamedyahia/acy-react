0c225bb601709fd55470805f0d7c979a7c342ee3
itsmohamedyahia <justmyahia@gmail.com>
2023-10-30 21:06:19 +0200

    comment styled components till remove from app

--- src/styled.tsx
+++ src/styled.tsx
@@ -1,10 +1,10 @@
-// import styled from "@emotion/styled";
-import styled from "styled-components";
+// // import styled from "@emotion/styled";
+// import styled from "styled-components";
 
-export const Title = styled.h1`
-  font-size: 4rem;
-  align-self: center;
+// export const Title = styled.h1`
+//   font-size: 4rem;
+//   align-self: center;
 
-  font-weight: 600;
-  ${(props) => props.acss}
-`;
+//   font-weight: 600;
+//   ${(props) => props.acss}
+// `;
