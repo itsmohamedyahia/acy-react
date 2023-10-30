8322e1e6de274b6fab4316ba6238e2431735e246
itsmohamedyahia <justmyahia@gmail.com>
2023-10-30 21:11:46 +0200

    uncomment styled component Title and comment build issue only

--- src/styled.tsx
+++ src/styled.tsx
@@ -1,10 +1,14 @@
-// // import styled from "@emotion/styled";
-// import styled from "styled-components";
+// import styled from "@emotion/styled";
+import styled from "styled-components";
 
-// export const Title = styled.h1`
-//   font-size: 4rem;
-//   align-self: center;
+export const Title = styled.h1`
+  font-size: 4rem;
+  align-self: center;
 
-//   font-weight: 600;
-//   ${(props) => props.acss}
-// `;
+  font-weight: 600;
+ 
+`;
+
+
+//  ${(props) => props.acss}
+// this was with Title styles
