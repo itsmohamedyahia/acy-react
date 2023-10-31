cc120ba4d42e26c8a882ed3828574985f35610ab
itsmohamedyahia <justmyahia@gmail.com>
2023-10-31 18:54:49 +0200

    add mob nav

---
+++ src/common/Layout/MobileNavbar/MobileNavbar.tsx
@@ -0,0 +1,16 @@
+import Home__Analytics from "../../../pages/Home/components/Home__Analytics"
+import Side__AnalyticsIcon from "../Sidebar/Components/Icons/Side__AnalyticsIcon"
+import Side_HomeIcon from "../Sidebar/Components/Icons/Side__HomeIcon"
+import s from "./MobileNavbar.module.css"
+
+
+export default function MobileNavbar() {
+    return (
+    <div className={s.mobile_nav} >
+        <Side_HomeIcon padding w={8} h={8}/>
+        <Side__AnalyticsIcon w={8}/>
+        <Side__AnalyticsIcon w={8}/>
+        <Side__AnalyticsIcon w={8}/>
+    </div>
+    )
+}
--- src/common/Layout/Root/Root.tsx
+++ src/common/Layout/Root/Root.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
 import s from "./Root.module.css";
-
+import MobileNavbar from "../MobileNavbar/MobileNavbar";
 import Navbar from "../Navbar/Navbar";
 
 import Main from "../Main/Main";
@@ -10,8 +10,9 @@ export default function Root() {
     <div id={s["root-layout"]}>
       <Sidebar />
       <div className={s["root-container"]}>
-      <Navbar />
-      <Main outlet={<Outlet />}></Main>
+        <Navbar />
+        <Main outlet={<Outlet />}></Main>
+        <MobileNavbar/>
       </div>
     </div>
   );
