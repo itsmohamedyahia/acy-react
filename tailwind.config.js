f8baeb98b094dc165f27492a714f027a726fa63f
itsmohamedyahia <justmyahia@gmail.com>
2023-11-01 02:06:45 +0200

    media query tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {  
     extend: {},
+    screens: {
+      // => @media (max-width: 1535px) { ... }
+      'mob': {'max': '1535px'},
+      'tab': {'max': '1150px'},
+      'lap': {'max': '1535px'},
+      'lap': {'max': '1535px'},
+      'monit': {'max': '1535px'},
+     
+    }
 },
   plugins: []
 };
