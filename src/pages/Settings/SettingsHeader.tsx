51e0a5bf92353df710bb60c63b1a826a2e48469a
itsmohamedyahia <justmyahia@gmail.com>
2023-11-01 21:15:29 +0200

    opt

--- src/pages/Settings/SettingsHeader.tsx
+++ src/pages/Settings/SettingsHeader.tsx
@@ -1,7 +1,7 @@
 export default function SettingsHeader({heading, desc}) {
     return ( <div className="">
     <h2 className="font-semibold leading-7">{heading}</h2>
-    <p style={{ maxWidth: "40ch" }}>
+    <p className="max-w-[40ch]">
      {desc}
     </p>
   </div>)
