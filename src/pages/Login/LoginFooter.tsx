67c2d576ef3fecb9aa845ae1d205834ffea1a495
itsmohamedyahia <justmyahia@gmail.com>
2023-11-01 21:13:11 +0200

    a to react link

--- src/pages/Login/LoginFooter.tsx
+++ src/pages/Login/LoginFooter.tsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function LoginFooter() {
   return (
     <p className="mt-10 text-sm text-center text-gray-500">
       Not a member?{" "}
-      <a
-        href="#"
+      <Link
+        to="/signup"
         className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
       >
         Sign up
-      </a>
+      </Link>
     </p>
   );
 }
