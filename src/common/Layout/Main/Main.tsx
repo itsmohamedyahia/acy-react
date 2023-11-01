e76b26ade813381a174a3dafab431b7b4f91935f
itsmohamedyahia <justmyahia@gmail.com>
2023-11-01 02:16:59 +0200

    clearing main.tsx

--- src/common/Layout/Main/Main.tsx
+++ src/common/Layout/Main/Main.tsx
@@ -1,10 +1,8 @@
-import React from "react";
 import s from "./Main.module.css";
 
 function Main(props) {
   return (
     <div className={s.main}>
-      {props.children}
       {props.outlet}
     </div>
   );
