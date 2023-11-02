a3c3958a6306aa45d9f6eda67056b29fb1511d62
itsmohamedyahia <justmyahia@gmail.com>
2023-11-02 16:52:45 +0200

    add x status and icons and whatever

--- src/pages/Home/components/HomeStreakDayStatus.tsx
+++ src/pages/Home/components/HomeStreakDayStatus.tsx
@@ -1,29 +1,25 @@
+import CheckIcon from "./CheckIcon";
+import XIcon from "./xIcon";
+
 export default function HomeStreakDayStatus({ status }) {
-  const statusCheckedCss = "bg-green-500";
-  const statusNotCheckedCss = "bg-green-100 border border-green-500";
+  const statusPresentCss = "bg-green-500";
+  const statusYetCss = "bg-green-100 border border-green-500";
+  const statusAbsentCss = "bg-red-500";
 
   return (
     <div
       className={
         "flex items-center justify-center w-6 h-6 rounded-full" +
         " " +
-        (status ? statusCheckedCss : statusNotCheckedCss)
+        (status === 0
+          ? statusYetCss
+          : status === 1
+          ? statusPresentCss
+          : statusAbsentCss)
       }
     >
-      {status && (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-4 text-white"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <polyline points="20 6 9 17 4 12"></polyline>
-        </svg>
-      )}
+      {status === 1 && <CheckIcon />}
+      {status === -1 && <XIcon color="white" />}
     </div>
   );
 }
--- src/pages/Home/components/HomeStreakDays.tsx
+++ src/pages/Home/components/HomeStreakDays.tsx
@@ -2,7 +2,7 @@ import HomeStreakDay from "./HomeStreakDay";
 
 export default function HomeStreakDays() {
   const days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-  const daysStatus = [true, true, true, false, false, true];
+  const daysStatus = [1, 1, -1, 1, 0, 0];
 
   return (
     <div className="flex items-center justify-between gap-x-10">
