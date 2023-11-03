04c2cd6b052a57de261688a56e4f3770e5520101
itsmohamedyahia <justmyahia@gmail.com>
2023-11-03 13:06:56 +0200

    fix bug

--- src/pages/Home/components/Leaderboard/Home__Leaderboard.tsx
+++ src/pages/Home/components/Leaderboard/Home__Leaderboard.tsx
@@ -5,7 +5,7 @@ import TableHead from "./components/Table__Head";
 export default function Home__Leaderboard() {
   return (
     <div className="flex flex-col home__leaderboard ">
-      <div className="p-1.5 min-w-full inline-block align-middle">
+      <div className="inline-block min-w-full align-middle ">
         <div className="overflow-hidden border rounded-lg shadow dark:border-gray-700 dark:shadow-gray-900">
           <table
            className="flex flex-col items-center min-w-full divide-y divide-gray-200 dark:divide-gray-700"
