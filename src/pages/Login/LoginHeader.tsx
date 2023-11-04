ea3fe774b83ce8915af0f1aa44ab23e78523b631
itsmohamedyahia <justmyahia@gmail.com>
2023-11-04 03:55:53 +0200

    h3 fix due to new change

--- src/pages/Login/LoginHeader.tsx
+++ src/pages/Login/LoginHeader.tsx
@@ -5,7 +5,7 @@ export default function LoginHeader({ text }) {
   return (
     <div className="w-full max-w-sm mx-auto">
       <LogoIcon className="w-auto h-12 mx-auto text-blue-700" />
-      <H3 text={text} className="mt-10 text-center" />
+      <H3 className="mt-10 text-center">{text}</H3>
     </div>
   );
 }
