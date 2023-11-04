36d30076febdaf5f40d342189c3bd4444b0eadc5
itsmohamedyahia <justmyahia@gmail.com>
2023-11-04 16:19:46 +0200

    new component mainContainer

---
+++ src/common/UI/Containers/MainContainer.tsx
@@ -0,0 +1,7 @@
+export default function MainContainer({ children }) {
+  return (
+    <div className="sm:p-6 py-[2.4rem] px-[10rem] 2xl:py-[1.6rem] 2xl:px-[3rem]">
+      {children}
+    </div>
+  );
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,17 +4,19 @@ import Home__Start from "./components/HomeStart/Home__Start";
 import Home__Leaderboard from "./components/Leaderboard/Home__Leaderboard";
 import Home__Streak from "./components/HomeStreak/Home__Streak";
 import Home__Analytics from "./components/HomeAnalytics/Home__Analytics";
+import MainContainer from "../../common/UI/Containers/MainContainer";
 
 export default function HomePage() {
   return (
-    <div className=" home sm:p-6">
-      <Home__Hello />
-      <Home__Start />
-      <Home__Leaderboard />
-      <Home__Streak />
-      <Home__Analytics />
-      {/* JUST FOR TESTING SCROLL ON MAIN EL */}
-      {/* <div>
+    <MainContainer>
+      <div className=" home">
+        <Home__Hello />
+        <Home__Start />
+        <Home__Leaderboard />
+        <Home__Streak />
+        <Home__Analytics />
+        {/* JUST FOR TESTING SCROLL ON MAIN EL */}
+        {/* <div>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, nostrum
         temporibus cupiditate totam nihil mollitia reiciendis similique quaerat
         blanditiis facere consectetur neque a, nobis laborum esse eum libero
@@ -74,6 +76,7 @@ export default function HomePage() {
         blanditiis facere consectetur neque a, nobis laborum esse eum libero
         inventore quod?
       </div> */}
-    </div>
+      </div>
+    </MainContainer>
   );
 }
