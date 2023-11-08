d8908bf21488c209f8cacd9bd0c300ac47c45b3d
itsmohamedyahia <justmyahia@gmail.com>
2023-11-08 06:09:40 +0200

    new icon

---
+++ src/common/UI/Icons/RightPlainArrowIcon.tsx
@@ -0,0 +1,22 @@
+export default function RightPlainArrowIcon({size}) {
+  const width = `${size/4}rem`
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className=""
+      width={width}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+    >
+      <path
+        d="M10 7L15 12L10 17"
+        stroke=""
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
