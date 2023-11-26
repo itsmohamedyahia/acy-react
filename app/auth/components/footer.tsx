e88e91a2d9e4bcaa60ae692a474ea9cc1a554c8a
itsmohamedyahia <justmyahia@gmail.com>
2023-11-27 01:02:06 +0200

    type checking add to auth comps

--- app/auth/components/footer.tsx
+++ app/auth/components/footer.tsx
@@ -1,4 +1,12 @@
-export default function AuthFooter({ children, anchor }) {
+import { ReactNode } from "react";
+
+export default function AuthFooter({
+  children,
+  anchor
+}: {
+  children: ReactNode;
+  anchor: ReactNode;
+}) {
   return (
     <p className="mt-10 text-sm text-center text-gray-500">
       <span className="mr-1.5">{children}</span>
--- app/auth/components/link.tsx
+++ app/auth/components/link.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export default function AuthLink({ to, text }) {
+export default function AuthLink({ to, text }: { to: string; text: string }) {
   return (
     <a
       href={to}
