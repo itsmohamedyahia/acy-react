67c6ee5b1af7110d667f1189c2ba975ce8deeaa8
itsmohamedyahia <justmyahia@gmail.com>
2023-11-29 16:18:04 +0200

    redirect to backend

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   async redirects() {
     return [
       {
         source: "/",
         destination: "/dashboard",
+        permanent: true
+      },
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:4000/:path*",
         permanent: true
       }
     ];
