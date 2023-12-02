13673df95de4d0b46c7256108576592221311a41
itsmohamedyahia <justmyahia@gmail.com>
2023-12-02 19:41:19 +0200

    button ts and opt

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,17 +1,29 @@
-export default function Button({ className = "", children, variant = "dark" }) {
-  const variantCss =
-    variant === "dark"
-      ? "text-white bg-blue-700 hover:bg-blue-800"
-      : "text-gray-800 bg-white hover:bg-gray-50 border ";
+import { ReactNode } from "react";
 
-  const buttonCss =
-    "focus:ring-2 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5  focus:outline-none " +
-    " " +
-    className +
-    " ";
+type ButtonProps = {
+  className?: string;
+  children: ReactNode;
+  variant?: "light" | "dark";
+};
+
+export default function Button({
+  className,
+  children,
+  variant = "dark"
+}: ButtonProps) {
+  const darkVariantCss = "text-white bg-blue-700 hover:bg-blue-800";
+  const lightVariantCss = "text-gray-800 bg-white hover:bg-gray-50 border";
+
+  const variantCss = variant === "dark" ? darkVariantCss : lightVariantCss;
+
+  const buttonCss = [
+    "focus:ring-2 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5  focus:outline-none",
+    className,
+    variantCss
+  ].join(" ");
 
   return (
-    <button type="submit" className={buttonCss + variantCss}>
+    <button type="submit" className={buttonCss}>
       {children}
     </button>
   );
