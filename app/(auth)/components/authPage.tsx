b66b1678a6ab12251a9fc65906ee6f39b845bcc1
itsmohamedyahia <justmyahia@gmail.com>
2023-12-11 17:48:33 +0200

    refactor auth pages, sign up working

---
+++ app/(auth)/components/authPage.tsx
@@ -0,0 +1,50 @@
+import Button from "@/ui/button";
+import Form from "@/ui/form/form";
+import FormControl from "@/ui/form/components/control";
+import FormLabel from "@/components/ui/form/components/label";
+import FormInput from "@/components/ui/form/components/input";
+import AuthHeader from "../components/header";
+import AuthFooter from "../components/footer";
+import AuthLink from "../components/link";
+import { FormEventHandler } from "react";
+
+type FormField = {
+  id: string;
+  label: string;
+};
+
+type configObj = {
+  headerText: string;
+  formFields: Array<FormField>;
+  buttonText: string;
+  footerText: string;
+  linkText: string;
+  linkHref: string;
+  onSubmit: FormEventHandler;
+};
+
+export default function AuthPage({ configObj }: { configObj: configObj }) {
+  return (
+    <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
+      <AuthHeader text={configObj.headerText} />
+      <div className="w-full max-w-sm mx-auto mt-10">
+        <Form className="space-y-6" onSubmit={configObj.onSubmit}>
+          {configObj.formFields.map((field) => (
+            <FormControl>
+              <FormLabel label={field.label} id={field.id} />
+              <FormInput id={field.id} />
+            </FormControl>
+          ))}
+          <Button className="w-full">{configObj.buttonText}</Button>
+        </Form>
+        <AuthFooter
+          anchor={
+            <AuthLink to={configObj.linkHref} text={configObj.linkText} />
+          }
+        >
+          {configObj.footerText}
+        </AuthFooter>
+      </div>
+    </div>
+  );
+}
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import Button from "@/ui/button";
-import Form from "@/ui/form/form";
-import FormControl from "@/ui/form/components/control";
-import AuthHeader from "../components/header";
-import AuthFooter from "../components/footer";
-import AuthLink from "../components/link";
 import { useRouter } from "next/navigation";
-import FormLabel from "@/components/ui/form/components/label";
-import FormInput from "@/components/ui/form/components/input";
-import { FormEvent, useState } from "react";
+import { FormEvent } from "react";
 import comAuthData from "../logic/onSubmit";
+import AuthPage from "../components/authPage";
 
 export default function Signup() {
   const router = useRouter();
@@ -28,23 +21,15 @@ export default function Signup() {
     { label: "Password", id: "passwd" }
   ];
 
-  return (
-    <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
-      <AuthHeader text="Create your account" />
-      <div className="w-full max-w-sm mx-auto mt-10">
-        <Form className="space-y-6" onSubmit={onSubmit}>
-          {formFields.map((field) => (
-            <FormControl>
-              <FormLabel label={field.label} id={field.id} />
-              <FormInput id={field.id} />
-            </FormControl>
-          ))}
-          <Button className="w-full">Sign Up</Button>
-        </Form>
-        <AuthFooter anchor={<AuthLink to="/login" text="Login" />}>
-          Already a member?
-        </AuthFooter>
-      </div>
-    </div>
-  );
+  const configObj = {
+    headerText: "Create your account",
+    formFields: formFields,
+    buttonText: "Sign Up",
+    footerText: "Already a member?",
+    linkText: "Login",
+    linkHref: "/login",
+    onSubmit: onSubmit
+  };
+
+  return <AuthPage configObj={configObj} />;
 }
