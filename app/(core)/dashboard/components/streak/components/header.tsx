f2bd0b3281d459ec0093516e431b5e9eca8249c0
itsmohamedyahia <justmyahia@gmail.com>
2023-12-15 11:41:12 +0200

    apostrophe to html entity

--- app/(core)/dashboard/components/streak/components/header.tsx
+++ app/(core)/dashboard/components/streak/components/header.tsx
@@ -1,5 +1,3 @@
 export default function HomeStreakHeader() {
-  return (
-    <h1 className="text-xl font-semibold">You're on a streak!</h1>
-  );
+  return <h1 className="text-xl font-semibold">You&#39;re on a streak!</h1>;
 }
